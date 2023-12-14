fa72609df485de409e8b22f2a84c03fbce9a3e4b
jachithra.cs <jachithra.cs@ust.com>
2023-12-14 17:55:05 +0530

    uploaded

---
+++ API_Assignments/CaseStudy_1/Models/BookingDates.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy_1.Models
+{
+    public class BookingDates
+    {
+        [JsonProperty("checkin")]
+        public string? CheckIn { get; set; }
+        [JsonProperty("checkout")]
+        public string? CheckOut { get; set; }
+    }
+}
